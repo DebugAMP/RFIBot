d84e662a887d1d828d83d94e646ff2b82df3ec1d
DebugAMP <43733429+DebugAMP@users.noreply.github.com>
2018-09-30 21:44:03 -0700

    Add files via upload
    Update allows multiple threads per GM and restricts all commands to GM only except disp and readspoiler

--- bot.js
+++ bot.js
@@ -72,11 +72,12 @@ function Entry(name, initmod, isuser) {
 	};
 }
 
-function Game(GM, noGMThread) {
+function Game(GM, GameID) {
+	GameID = parseInt(GameID);
 	this.GM = GM;
 	this.entries = new Array();
 	this.spoilers = new Array();
-	this.noGMThread = (noGMThread == null ? false : noGMThread);
+	this.GameID = (GameID == null ? 0 : GameID);
 }
 
 var games = new Array();
@@ -100,97 +101,79 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, PREFIX_LENGTH) == PREFIX) {
-		if (message == PREFIX + 'startgame') {
-			if (games[channelID] == null) {
-				var alreadyHasGame = false;
-				for (var x in games) {
-					if (games[x].GM == userID) {
-						alreadyHasGame = true;
-						break;
+    if (message.substring(0, PREFIX_LENGTH).toLowerCase() == PREFIX) {
+		var args = message.substring(PREFIX_LENGTH).split(' ');
+		var cmd = '';
+		var game = null;
+		var out_msg = '';
+		
+		//Idle Commands
+		if (args[0] == 'i' || args[0] == 'idle') {
+			cmd = args[1];
+			logger.info('Idle command: ' + cmd);
+			switch (cmd) {
+				case 'startgame':
+					if (games[channelID] == null) {
+						var gameID = 0;
+						for (var x in games) if (games[x].GM == userID) gameID++;
+						games[channelID] = new Game(userID, gameID);
+						out_msg = 'New game started!  GM: ' + userID + ', Game ID: ' + gameID;		
 					}
-				}
-				if (alreadyHasGame) {
-					out_msg = 'Warning, GM already owns a game.  GM thread feature will not function on this game.';
-					games[channelID] = new Game(userID, true);
+					else out_msg = 'Game already started';
+				break;
+				case 'endgame':
+					if (games[channelID].GM == userID) {
+						delete(games[channelID]);
+						out_msg = 'Game ended';
+					}
+					else out_msg = 'You are not the GM, you cannot end this game.';
+				break;
+				case 'ping':
+					out_msg = 'RollForInitiative PbD Helper Bot reporting for duty!';
+				break;
+			}
+		}
+		
+		//Game Commands
+		else {
+			var entry_name, param_name, value, cmd_mode;
+			
+			//GM Commands
+			if (args[0] == 'g' || args[0] == 'gm') {
+				var gameID = parseInt(args[1]);
+				if ((games[channelID] != null) && (games[channelID].GM == userID)) {
+					game = games[channelID];
+					args = args.splice(1);
 				}
 				else {
-					games[channelID] = new Game(userID);
-					out_msg = 'New game started!';
-				}			
-			}
-			else out_msg = 'Game already started';
-		}
-		else if (message == PREFIX + 'endgame') {
-			if (games[channelID].GM == userID) {
-				delete(games[channelID]);
-				out_msg = 'Game ended';
-			}
-			else out_msg = 'You are not the GM';
-		}
-		else {
-			var args = message.substring(PREFIX_LENGTH).split(' ');
-			var cmd = args[0].toLowerCase();
-			var entry_name, param_name, value;
-			var out_msg = '';
-			
-			var entries;
-			var spoilers;
-			
-			if (games[channelID] != null) {
-				entries = games[channelID].entries;
-				spoilers = games[channelID].spoilers;
-			}
-			else {
-				for (var x in games) {
-					if (games[x].GM == userID && ! games[x].noGMThread) {
-						entries = games[x].entries;
-						spoilers = games[x].spoilers;
-						break;
-					}
+					for (var x in games) if (games[x].GM == userID && games[x].GameID == gameID) game = games[x];
+					args = args.splice(2);
 				}
+				cmd_mode = 'GM';
 			}
-		   
-			//args = args.splice(1);
-			if (entries != null) switch(cmd) {
-				// !ping
-				case 'ping':
-					out_msg = channelID;
-				break;
-				
+			
+			//Player Commands
+			else {
+				game = games[channelID];
+				args = args.splice(1);
+				cmd_mode = 'PC';
+			}
+			
+			if (game == null) return;
+			
+			var entries = game.entries;
+			var spoilers = game.spoilers;
+			cmd = args[0];
+			logger.info('Game command: ' + cmd);
+			
+			if (cmd_mode == 'GM') switch(cmd) {
+			
+				// Test command to load sample roster
 				case 'test':
 					for (var x = 0; x < 5; x++) {
 						entries.push(new Entry('monster' + x, x));
 					}
 					out_msg = 'Test list loaded';
-				break;
-				
-				case 'addme':
-				case 'new':
-				// Arguments: name (optional), initiative modifier
-					out_msg = 'No action taken';
-					var isuser;
-					if (cmd == 'addme') {
-						entry_name = user.toLowerCase();
-						value = args[1];
-						isuser = 1;
-					}
-					else {
-						entry_name = args[1];
-						value = args[2];
-						isuser = 0;
-					}
-					var found = false;
-					for (var x = 0; x < entries.length; x++) {
-						if (entries[x].params["name"] == entry_name) {
-							found = true;
-							break;
-						}
-					}
-					if (found == false) {
-						entries.push(new Entry(entry_name, value, isuser));
-						out_msg = 'Added new player, ' + entry_name + ', with init modifier of ' + value;
-					}
 				break;
 				
 				case 'drop':
@@ -204,15 +187,28 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					}
 				break;
 				
-				case 'disp':
-				// Arguments: none
-					for (var x = 0; x < entries.length; x++) {
-						out_msg += entries[x].display(); 
+				case 'new':
+				// Arguments: name (optional), initiative modifier
+					entry_name = args[1];
+					value = args[2];
+					var found = false;
+					for (var x in entries) {
+						if (entries[x].params["name"] == entry_name) {
+							found = true;
+							out_msg = 'Character already exists';
+							break;
+						}
+					}
+					if (found == false) {
+						entries.push(new Entry(entry_name, value));
+						out_msg = 'Added new player, ' + entry_name + ', with init modifier of ' + value;
 					}
 				break;
 				
+				// Modify parameter values either by setting equal to, adding, subtracting, or creating
 				case 'set':
 				case 'add':
+				case 'sub':
 				case 'newparam':
 				// Arguments: name, parameter, value
 					entry_name = args[1];
@@ -222,6 +218,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 						if (entries[x].params["name"] == entry_name) {
 							if (cmd == 'set') entries[x].set(param_name, value);
 							else if (cmd == 'add') entries[x].add(param_name, value);
+							else if (cmd == 'sub') entries[x].add(param_name, (value * -1));
 							else if (cmd == 'newparam') entries[x].new_param(param_name, value);
 							out_msg = entries[x].display();
 						}
@@ -234,14 +231,17 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					if (cmd == 'act') value = 1;
 					else value = 0;
 					args = args.slice(1);
-					if (args.length == 0)
-						for (var x in entries)
-							entries[x].set("active", value);
-					else
-						for (var x in args)
-							for (var y in entries)
-								if (entries[y].params["name"] == args[x].toLowerCase())
+					if (args.length == 0) for (var x in entries) entries[x].set("active", value);
+					else {
+						for (var x in args) {
+							for (var y in entries) {
+								if (entries[y].params["name"] == args[x].toLowerCase()) {
 									entries[y].set("active", value);
+									out_msg += entries[x].display();
+								}
+							}
+						}
+					}
 				break;
 				
 				case 'rollall':
@@ -252,23 +252,35 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					entries.sort(function(entry1, entry2) {
 						return entry2.params["init"] - entry1.params["init"];
 					});
+					out_msg += '\n';
+					cmd = 'disp';
 				break;
 				
 				case 'addspoiler':
 				// Arguments: spoiler name, spoiler
-					spoilers[args[1]] = message.substring(PREFIX_LENGTH + 11 + args[1].length);
+					spoilers[args[1]] = message.substring(PREFIX_LENGTH + 13 + args[1].length);
+				break;
+				
+				case 'dropspoiler':
+				// Arguments: spoiler name
+					delete(spoilers[args[1]]);
+				break;
+			}
+				
+			switch(cmd) {
+				case 'disp':
+				// Arguments: none
+					for (var x in entries) {
+						out_msg += entries[x].display(); 
+					}
 				break;
 				
 				case 'readspoiler':
 				// Arguments: spoiler name
 					out_msg = spoilers[args[1]];
 				break;
-				
-				case 'dropspoiler':
-				// Arguments: spoiler name
-					delete(spoilers[args[1]]);
-			 }
-		 }
-		 bot.sendMessage({to: channelID, message: out_msg});
+			}
+		}
+		bot.sendMessage({to: channelID, message: out_msg});
 	}
 });
